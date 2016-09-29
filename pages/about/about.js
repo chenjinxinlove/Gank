46c295bbfcebef1515ccb603154d206ca5d29e7f
陈金鑫 <505150704@qq.com>
2016-09-29 12:48:32 +0800

    three

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,20 +1,19 @@
 Page({
   data:{
-    // text:"这是一个页面"
+    actionSheetHidden: true,
+    actionSheetItems: ['item1', 'item2', 'item3', 'item4']
   },
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  actionSheetTap: function(e) {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    })
   },
-  onReady:function(){
-    // 页面渲染完成
+  actionSheetChange: function(e) {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    })
   },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  bindItemTap:function(e){
+    console.log('tap ' + e.currentTarget.dataset.name)
   }
 })
--- pages/add/add.js
+++ pages/add/add.js
@@ -1,7 +1,16 @@
+var PostData = require('../../js/PostData.js') ;
 Page({
   data:{
     urlClass : " ",
     whoClass : " ",
+    typeClass :" ",
+    descClass : " ",
+    addTypeDis : false,
+    actionType : true,
+    actionSheetItems : ['Android ','iOS','休息视频','福利','拓展资源','前端','瞎推荐','App'],
+    addTypeData : "",
+    callbackDesc : " ",
+    toast1Hidden : true,
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
@@ -9,8 +18,7 @@ Page({
   // 验证是否为http
   verifyHtpp : function (event){
       var value = event.detail.value;
-      var regex = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/;
-      if(!regex.test(value)){
+      if(verifyHttp(value)){
          this.setData({
             "urlClass" : "urlClass"
           })
@@ -33,5 +41,97 @@ Page({
             "whoClass" : " "
           }) 
     }
-  }
+  },
+  verifyNotD : function(event){
+    var value = event.detail.value;
+    if(value.length === 0){
+       this.setData({
+            "descClass" : "descClass"
+          })
+    }else{
+       this.setData({
+            "descClass" : " "
+          }) 
+    }
+  },
+
+  // 类型验证
+  addType : function(event){
+    this.setData({
+       addTypeDis : true,
+       actionType :false,
+    })
+  },
+
+  bindItemTap:function(e){
+    this.setData({
+       addTypeDis :false,
+       actionType :true,
+       addTypeData :e.currentTarget.dataset.name,
+    })
+  },
+
+  actionSheetChange: function(e) {
+    this.setData({
+       addTypeDis : false,
+       actionType :true,
+    })
+  },
+  hiddenToast : function(){
+      this.setData({
+                toast1Hidden :true,
+              })
+  },
+  // 表单提交
+  formSubmit: function(e) {
+    var _self = this;
+    var valueAll = e.detail.value;
+    if(verifyHttp(valueAll.url)){
+      this.setData({
+            "urlClass" : "urlClass"
+          })
+    }
+    if(valueAll.who.length === 0){
+      this.setData({
+            "whoClass" : "whoClass"
+          })
+    }
+    if(valueAll.type.length === 0){
+      this.setData({
+            "typeClass" : "typeClass"
+          })
+    }
+    if(valueAll.desc.length === 0){
+      this.setData({
+            "descClass" : "descClass"
+          })
+    }
+    if(!verifyHttp(valueAll.url) && valueAll.who.length > 0 && valueAll.type.length > 0 && valueAll.desc.length > 0){
+      valueAll['debug'] = 'true';
+      var options = {
+        'data' : valueAll
+      }
+      PostData(options,function(res){
+          if(res.data && !res.data.error){
+               _self.setData({
+                toast1Hidden :false,
+                callbackDesc : res.data.msg
+              })
+           }else{
+              _self.setData({
+                toast1Hidden :false,
+                callbackDesc : res.data.msg
+              })
+           }   
+      })
+    }
+
+  },
 })
+
+
+
+function verifyHttp(value){
+  var regex = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/;
+  return !regex.test(value)
+}
