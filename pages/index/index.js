c74a2249270466d3642366c6c8fbae52df9c9eb1
陈金鑫 <505150704@qq.com>
2016-09-29 13:37:32 +0800

    four

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,10 +8,19 @@ Page({
     GankDatas:[],
     loadingHidden : false,
     modalHidden : true,
-    bodyHeight : 0
+    bodyHeight : 0,
+    userInfo: {}
   },
   onLoad:function(options){
-    reda(this, '福利')
+    reda(this, '福利');
+    var that = this
+    //调用应用实例的方法获取全局数据
+    appInstance.getUserInfo(function(userInfo){
+    //更新数据
+      that.setData({
+        userInfo:userInfo
+     })
+    })
   },
   onReady:function(){
     // 页面渲染完成
